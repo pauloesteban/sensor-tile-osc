c0a67a650240cef68b0484e93ad3e22a46b84bd6
Paulo Chiliguano <12860112+pauloesteban@users.noreply.github.com>
2022-08-14 09:22:54 -0500

    Populate listbox

--- metabow_bridge.py
+++ metabow_bridge.py
@@ -12,18 +12,15 @@
 # 2020
 import asyncio
 import tkinter as tk
-
 from datetime import datetime
 from functools import partial
-from typing import Union  # For later use with type hint on running loop, see below
-
+from tkinter import ttk
 from bleak import (
     BleakClient,
     BleakScanner,
 )
 
 from pythonosc import udp_client
-
 from gesture_model import GestureModel
 from utils import bytearray_to_fusion_data, log_file_path
 
@@ -33,13 +30,6 @@ CHARACTERISTIC_UUID = "00E00000-0001-11E1-AC36-0002A5D5C51B"
 st_devices = {}
 filename = log_file_path()
 model = GestureModel()
-
-
-def device_found(device, _):
-    if device.name == "AM1V330":
-        print(f"{device.__dict__}\033[92m found\033[0m {type(device)}")
-
-        st_devices[device.address] = device  # bleak.backends.device.BLEDevice
 
 
 def notification_handler(device_number:int, sender: int, data: bytearray):
@@ -74,58 +64,60 @@ async def main():
     await asyncio.gather(*(connect_to_device(i, device) for i, (_, device) in enumerate(st_devices.items())))
 
 
+def device_found(device, _):
+    if device.name == "AM1V330":
+        print(f"{device.__dict__}\033[92m found\033[0m {type(device)}")
+    st_devices[device.address] = device  # bleak.backends.device.BLEDevice
+
+
 class Window(tk.Tk):
     def __init__(self, loop: asyncio.unix_events._UnixSelectorEventLoop):  # Delete type hint when building on Windows
         self.root = tk.Tk()
         self.root.title("Metabow OSC bridge")
         self.root.resizable(False, False)
-
         self.loop = loop
-        self.scanner = BleakScanner()
-
-        title = tk.Label(text="OSC Port")
-        title.grid(row=1, column=0, padx=(8, 8), pady=(8, 8))
-
-        osc_port = tk.Entry(
-            self.root,
-        )
-
-        osc_port.grid(row=1, column=1, sticky=tk.W, padx=8, pady=8)
-
-        title = tk.Label(text="Devices")
-        title.grid(row=2, column=0, padx=(8, 8), pady=(16, 0))
-
+        self.scanner = BleakScanner()  # TODO: Catch error when Bluetooth is off
+        title = ttk.Label(text="OSC Port")
+        title.grid(row=0, column=0, padx=(8, 8), pady=(8, 8))
+        osc_port = ttk.Entry(self.root,)
+        osc_port.grid(row=0, column=1, sticky=tk.W, padx=8, pady=8)
+        title = ttk.Label(text="Devices")
+        title.grid(row=1, column=0, padx=(8, 8), pady=(16, 0))
         self.devices = tk.Listbox()
-        self.devices.grid(row=3, rowspan=4, columnspan=3, padx=8, pady=8)
+        self.devices.grid(row=2, rowspan=4, columnspan=3, padx=8, pady=8)
         self.devices.insert(0, "Please press Start Scan...")
-
-        start_scan_button = tk.Button(
+        self.start_scan_button = ttk.Button(
             text="Start Scan",
             width=10,
             command=lambda: self.loop.create_task(self.start_scan()),
         )
         
-        start_scan_button.grid(row=3, column=3, sticky=tk.W, padx=8, pady=8)
-
-        stop_scan_button = tk.Button(
+        self.start_scan_button.grid(row=2, column=3, sticky=tk.W, padx=8, pady=8)
+        self.start_scan_button.focus()
+        self.stop_scan_button = ttk.Button(
             text="Stop Scan",
             width=10,
             command=lambda: self.loop.create_task(self.stop_scan()),
         )
 
-        stop_scan_button.grid(row=4, column=3, sticky=tk.W, padx=8, pady=8)
-
-        connect_button = tk.Button(
+        self.stop_scan_button.state(['disabled'])
+        self.stop_scan_button.grid(row=3, column=3, sticky=tk.W, padx=8, pady=8)
+        connect_button = ttk.Button(
             text="Connect",
             width=10,
             # command=lambda: self.loop.create_task(self.start_scan()),
         )
         
-        connect_button.grid(row=5, column=3, sticky=tk.W, padx=8, pady=8)
+        connect_button.state(['disabled'])
+        connect_button.grid(row=4, column=3, sticky=tk.W, padx=8, pady=8)
+        self.refresh_listbox = False
 
 
     async def show(self):
         while True:
+            if self.refresh_listbox:
+                self.populate_devices()
+
             self.root.update()
             await asyncio.sleep(0.1)
 
@@ -133,22 +125,27 @@ class Window(tk.Tk):
     async def start_scan(self):
         self.scanner.register_detection_callback(device_found)
         await self.scanner.start()
-        await self.populate_devices()
+        self.start_scan_button.state(['disabled'])
+        self.stop_scan_button.state(['!disabled'])
+        self.refresh_listbox = True
         await asyncio.sleep(1.0)
     
 
     async def stop_scan(self):
+        self.start_scan_button.state(['!disabled'])
+        self.stop_scan_button.state(['disabled'])
+        self.refresh_listbox = False
         await self.scanner.stop()
-        await self.populate_devices()
 
 
-    async def populate_devices(self):
-        self.devices.delete(0, 'end')
+    def populate_devices(self):
+        if len(st_devices):
+            self.devices.delete(0, 'end')
 
-        for k in st_devices:
-            self.devices.insert('end', k)
+            for k in st_devices:
+                self.devices.insert('end', k)
 
-        await asyncio.sleep(1.0)
+        # await asyncio.sleep(0.0)
 
 
 async def metabow():
@@ -157,5 +154,4 @@ async def metabow():
 
 
 if __name__ == '__main__':
-
     asyncio.run(metabow())
